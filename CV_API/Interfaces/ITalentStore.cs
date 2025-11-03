59ef7553f349400dfc392377cd575102bcd247b3
Daniel R. <danielrudcallesen96@gmail.com>
2025-11-03 09:40:23 +0100

    Interface for talents

---
+++ CV_API/Interfaces/ITalentStore.cs
@@ -0,0 +1,12 @@
+﻿using CV_API.Models;
+
+namespace CV_API.Interfaces
+{
+    public interface ITalentStore
+    {
+        IReadOnlyList<Talent> GetTalents();
+        Talent? GetTalent(Guid id);
+        IReadOnlyList<Document> GetDocuments(Guid talentId);
+        Document? GetDocument(Guid talentId, Guid documentId);
+    }
+}
