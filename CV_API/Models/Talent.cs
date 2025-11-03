749471cda52254fa508308601ba4466f870ca5ed
Daniel R. <danielrudcallesen96@gmail.com>
2025-11-03 09:33:44 +0100

    Add project files.

---
+++ CV_API/Models/Talent.cs
@@ -0,0 +1,27 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Text.Json.Serialization;
+
+namespace CV_API.Models
+{
+    public class Talent
+    {
+        public Guid Id { get; set; }
+        [Required]
+        public string Name { get; set; } = default;
+        [Required]
+        public string Title { get; set; } = default;
+
+        [JsonPropertyName("profile_text")]
+        public string? ProfileText { get; set; }
+
+        [EmailAddress]
+        public string? Email { get; set; }
+        
+        public string? Phone { get; set; }
+        public string? City { get; set; }
+        public string? Country { get; set; }
+
+        public Uri? Github { get; set; }
+        public Uri? LinkedIn { get; set; }
+    }
+}
